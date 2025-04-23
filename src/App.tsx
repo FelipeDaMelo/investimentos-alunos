7eab7fdcf152a9324ee294c7796769c77c717e64
Felipe Damas Melo <felipedamelo@gmail.com>
2025-04-23 15:52:42 -0300

    Commit Fire

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,7 @@
-//src/App.tsx
-import React, { useState, useCallback } from 'react';
-import './index.css'
-import Login from './Login';
-import MainPage from './MainPage';
-
-const App = () => {
-  const [valorInvestido, setValorInvestido] = useState<number>(0);
-  const [fixo, setFixo] = useState<number>(0);
-  const [variavel, setVariavel] = useState<number>(0);
-  const [nomeGrupo, setNomeGrupo] = useState<string>('');
-  const [login, setLogin] = useState<string | null>(null);
-
-  const handleLogin = useCallback(
-    (valorInvestido: number, fixo: number, variavel: number, nomeGrupo: string) => {
-      setValorInvestido(valorInvestido);
-      setFixo(fixo);
-      setVariavel(variavel);
-      setNomeGrupo(nomeGrupo);
-      setLogin(nomeGrupo);
-    },
-    []
-  );
-
+function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      {!login ? (
-        <Login onLogin={handleLogin} />
-      ) : (
-        <MainPage 
-          login={login} 
-          valorInvestido={valorInvestido} 
-          fixo={fixo} 
-          variavel={variavel} 
-          nomeGrupo={nomeGrupo} 
-        />
-      )}
+    <div className="p-6 bg-green-500 text-white rounded-lg shadow-lg text-xl">
+      Tailwind está funcionando! 🎉
     </div>
   );
-};
-
-export default App;
+}
